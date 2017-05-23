834760efce0b9e88db132483ef3f7b67b9bea20a
Dion d'Aquino <daquinohd@users.noreply.github.com>
2017-05-23 15:07:57 -0400

    Adding response vars

--- TLSCanary/CanaryTest.cs
+++ TLSCanary/CanaryTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +9,33 @@ namespace TLSCanary
 {
     class CanaryTest
     {
+
+        private const String URL = "https://clinicaltrialsapi.cancer.gov";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Site status:");
-            Console.WriteLine("Something something...");
 
+            HttpWebResponse resp = GetResponse(URL);
+
+            Console.WriteLine("Status code: " + (int)resp.StatusCode + " " + resp.StatusDescription);
+            Console.WriteLine("Content length: " + resp.ContentLength);
+            Console.WriteLine("Content type: " + resp.ContentType);
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
+
+
+        private static HttpWebResponse GetResponse(string url)
+        {
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "HEAD";
+            var response = (HttpWebResponse)request.GetResponse();
+
+            // status code...
+            return response;
+        }
+
     }
 }

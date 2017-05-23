ecbf13d9d36939c27052e25814c86595a3d14b0c
Dion d'Aquino <daquinohd@users.noreply.github.com>
2017-05-23 14:17:47 -0400

    Adding console app files to source control

---
+++ TLSCanary/CanaryTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLSCanary
+{
+    class CanaryTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Site status:");
+            Console.WriteLine("Something something...");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
